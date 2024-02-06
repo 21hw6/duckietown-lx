2cccbba895ee92a368888b9a58a87964ccca6d5a
21hw6 <21hw6@queensu.ca>
2024-02-06 21:37:25 +0000

    "First iteration"

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,13 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    for i in range (0, 320):
+        res[240:, i] = (i)/3200
+
+    for i in range (320, 640):
+        res[240:, i] = -(640-i)/3200
+    # res[100:150, 100:150] = 1
+    # res[300:, 200:] = 1
     # ---
     return res
 
@@ -17,6 +22,12 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    for i in range (0, 320):
+        res[240:, i] = -i/3200
+
+    for i in range (320, 640):
+        res[240:, i] = (640-i)/3200
+    #res[100:150, 100:300] = -1
+    #res[:, :] = -0.01
     # ---
     return res
